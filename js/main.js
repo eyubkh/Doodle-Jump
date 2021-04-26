8e74b20b4de4e867419b7443f4f73485af08d58c
Eyub <eyub.kh@gmail.com>
2021-04-26 10:05:53 +0100

    Starting

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const doodler = document.createElement('div');
+
+    function createDoodler() {
+        grid.appendChild(doodler);
+        doodler.className = 'gird_doodler';
+    }
+    createDoodler();
+});
